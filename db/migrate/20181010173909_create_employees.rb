631d18b930823f886eaa986cb02b4c9c0117d752
babylee2002 <babylee2002@gmail.com>
2018-10-10 13:42:39 -0400

    setup done models created

---
+++ db/migrate/20181010173909_create_employees.rb
@@ -0,0 +1,12 @@
+class CreateEmployees < ActiveRecord::Migration[5.1]
+  def change
+    create_table :employees do |t|
+      t.string :name
+      t.string :postion
+      t.integer :age
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
---
+++ public/precache-manifest.651401b5c14da2cc847d08609cf131f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3142d8833862f5411ea1",
+    "url": "/static/js/main.3142d883.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "3142d8833862f5411ea1",
+    "url": "/static/css/main.2ec5f016.chunk.css"
+  },
+  {
+    "revision": "49f54fbcc19070f22e27c97cfa6c48a7",
+    "url": "/index.html"
+  }
+];
